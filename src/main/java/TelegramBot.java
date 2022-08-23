5acf75c060f0c84f55484281348c2d60c34af596
danillolec@gmail.com <dUg3wdmw>
2022-08-24 00:37:55 +0300

    bot

--- src/main/java/TelegramBot.java
+++ src/main/java/TelegramBot.java
@@ -1,9 +1,31 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TelegramBot extends TelegramLongPollingBot {
+
+
+
+    public TelegramBot() {
+        List<BotCommand> listofCommands = new ArrayList<>();
+        listofCommands.add(new BotCommand("/start", "Приветствие"));
+        listofCommands.add(new BotCommand("/menu", "выводит меню заведения"));
+        try {
+            this.execute(new SetMyCommands(listofCommands, new BotCommandScopeDefault(), null));
+        }
+        catch (TelegramApiException e) {
+        }
+    }
+
     @Override
     public String getBotUsername() {
         return "Sia_bot";
@@ -33,19 +55,42 @@ public class TelegramBot extends TelegramLongPollingBot {
                     } catch (TelegramApiException e) {
                         e.printStackTrace();
                     }
+
             }
         }
     }
+
+
 
     private void startCommandReceived(long chatId, String name) throws TelegramApiException {
         String answer = "Hi, " + name + " bro, what do u want?";
         sendMessage(chatId, answer);
     }
 
+
+
     private void sendMessage(long chatId, String textToSend) throws TelegramApiException {
         SendMessage message = new SendMessage();
         message.setChatId(chatId);
         message.setText(textToSend);
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardRow row = new KeyboardRow();
+        row.add("Меню");
+        row.add("Отзывы");
+
+        keyboardRows.add(row);
+        row = new KeyboardRow();
+        row.add("Предложения");
+        row.add("Расписание");
+        row.add("Как добраться");
+
+
+
+        keyboardRows.add(row);
+        keyboardMarkup.setKeyboard(keyboardRows);
+        message.setReplyMarkup(keyboardMarkup);
 
         try {
             execute(message);
